17cdc28a2747a8647b89b17be99ddc1fa9a004a0
Luxa2020 <141860695+Luxa2020@users.noreply.github.com>
2025-07-05 12:09:53 +0000

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -1,9 +1,1 @@
-import os
-from openai import OpenAI
-from dotenv import load_dotenv
 
-load_dotenv()  # Charge les variables depuis le fichier .env
-
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
-)
